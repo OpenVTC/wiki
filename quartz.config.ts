5231d7e3e0f5de8fcf394291e3329041a12bc7bd
Geoff Turk <geoff@ic3.dev>
2026-05-09 09:28:12 +0200

    Remove extra title

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,10 +13,10 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "null",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "geoffturk.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
